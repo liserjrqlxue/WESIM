8467d5325c5e3727d1aa6595ca86ecee6a4f7913
王垚燊(Yaoshen Wang) <wangyaoshen@genomics.cn>
2019-04-26 18:20:56 +0800

    use reference of array and struct to modify itself, add flowSteps, flowDown2UPs and flowUp2Downs

--- pipeline.go
+++ pipeline.go
@@ -8,8 +8,8 @@ import (
 type pipeline struct {
 	pipelineName string
 	version      string
-	steps        []PStep
-	files        []PFile
+	steps        *[]PStep
+	files        *[]PFile
 }
 
 type PStep struct {
@@ -23,7 +23,7 @@ type PStep struct {
 	ModuleIndex   int      `json:"moduleIndex"`
 	PriorStep     []string `json:"priorStep"`
 	NextStep      []string `json:"nextStep"`
-	JobSh         []PJob   `json:"jobSh"`
+	JobSh         *[]PJob  `json:"jobSh"`
 }
 
 func newPStep(name string) (step PStep) {
@@ -32,25 +32,29 @@ func newPStep(name string) (step PStep) {
 	step.ComputingFlag = "cpu"
 	step.Threads = 1
 	step.Timeout = 0
+	step.PriorStep = []string{}
+	step.NextStep = []string{}
+	step.JobSh = &[]PJob{}
 	return
 }
 
 type PJob struct {
-	computingFlag string
-	mem           int
-	sh            string
+	ComputingFlag string `json:"computingFlag"`
+	Mem           int    `json:"mem"`
+	Sh            string `json:"sh"`
 }
 
 func newPJob(mem int) (job PJob) {
-	job.computingFlag = "cpu"
+	job.Mem = mem
+	job.ComputingFlag = "cpu"
 	return
 }
 
 func (job *PJob) addSampleSh(workdir, sampleID, tag string) {
-	job.sh = filepath.Join(workdir, sampleID, "shell", strings.Join([]string{tag, "sh"}, ","))
+	job.Sh = filepath.Join(workdir, sampleID, "shell", strings.Join([]string{tag, "sh"}, ","))
 }
 func (job *PJob) addLaneSh(workdir, sampleID, laneName, tag string) {
-	job.sh = filepath.Join(workdir, sampleID, "shell", strings.Join([]string{tag, laneName, "sh"}, ","))
+	job.Sh = filepath.Join(workdir, sampleID, "shell", strings.Join([]string{tag, laneName, "sh"}, ","))
 }
 
 func (step *PStep) addSampleJobs(samples []string, workdir string, mem int) {
@@ -60,7 +64,7 @@ func (step *PStep) addSampleJobs(samples []string, workdir string, mem int) {
 		job.addSampleSh(workdir, sampleID, step.Name)
 		stepJobs = append(stepJobs, job)
 	}
-	step.JobSh = stepJobs
+	step.JobSh = &stepJobs
 }
 
 func (step *PStep) addLaneJobs(infoList map[string]info, workdir string, mem int) {
@@ -72,7 +76,32 @@ func (step *PStep) addLaneJobs(infoList map[string]info, workdir string, mem int
 			stepJobs = append(stepJobs, job)
 		}
 	}
-	step.JobSh = stepJobs
+	step.JobSh = &stepJobs
+}
+
+func flowSteps(steps ...*PStep) {
+	for i, step := range steps {
+		if i < len(steps)-1 {
+			step.NextStep = append(step.NextStep, steps[i+1].Name)
+		}
+		if i > 0 {
+			step.PriorStep = append(step.PriorStep, steps[i-1].Name)
+		}
+	}
+}
+
+func flowDown2Ups(downStep *PStep, upSteps ...*PStep) {
+	for _, upStep := range upSteps {
+		upStep.NextStep = append(upStep.NextStep, downStep.Name)
+		downStep.PriorStep = append(downStep.PriorStep, upStep.Name)
+	}
+}
+
+func flowUp2Downs(upStep *PStep, downSteps ...*PStep) {
+	for _, downStep := range downSteps {
+		upStep.NextStep = append(upStep.NextStep, downStep.Name)
+		downStep.PriorStep = append(downStep.PriorStep, upStep.Name)
+	}
 }
 
 type PFile struct {
