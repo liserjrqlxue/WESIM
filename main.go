38a02fe8bcacda5490c106a2bccaddaf10df4f25
王垚燊(Yaoshen Wang) <wangyaoshen@genomics.cn>
2019-04-26 10:46:31 +0800

    add test input.list and parse -input to struct

--- main.go
+++ main.go
@@ -5,12 +5,23 @@ import (
 	"github.com/liserjrqlxue/simple-util"
 	"log"
 	"os"
+	"path/filepath"
+	"strings"
+)
+
+// os
+var (
+	ex, _  = os.Executable()
+	exPath = filepath.Dir(ex)
+	pSep   = string(os.PathSeparator)
+	//dbPath       = exPath + pSep + "db" + pSep
+	//templatePath = exPath + pSep + "template" + pSep
 )
 
 var (
 	input = flag.String(
 		"input",
-		"",
+		exPath+pSep+"input.list",
 		"input samples info",
 	)
 )
@@ -49,11 +60,30 @@ func main() {
 		flag.Usage()
 		os.Exit(0)
 	}
-	sampleInfo, title := simple_util.File2MapArray(*input, "\t", nil)
+	sampleList, title := simple_util.File2MapArray(*input, "\t", nil)
 	checkTitle(title)
 	var infoList = make(map[string]info)
-	for _, item := range sampleInfo {
+	for _, item := range sampleList {
+		var sampleID = item["main_sample_num"]
+		var laneCode = item["lane_code"]
+		var fqPath = item["FQ_path"]
+		var pe = strings.Split(fqPath, ",")
+		if len(pe) != 2 {
+			log.Fatalf("can not parse pair end in lane(%s) of sample(%s):[%s]\n", laneCode, sampleID, fqPath)
+		}
+		var lane = laneInfo{
+			laneName: laneCode,
+			fq1:      pe[0],
+			fq2:      pe[1],
+		}
+		sampleInfo, ok := infoList[sampleID]
+		if !ok {
+			sampleInfo.SampleID = sampleID
+		}
+		sampleInfo.LaneInfo = append(sampleInfo.LaneInfo, lane)
+		infoList[sampleID] = sampleInfo
 	}
+	log.Printf("%+v\n", infoList)
 }
 
 func checkTitle(title []string) {
