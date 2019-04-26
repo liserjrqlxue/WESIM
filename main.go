8de89e57d8b044dd148548d4b50c9db77a4f17db
王垚燊(Yaoshen Wang) <wangyaoshen@genomics.cn>
2019-04-26 14:11:47 +0800

    add pipeline struct and crate step1 step2 step3

--- main.go
+++ main.go
@@ -24,6 +24,11 @@ var (
 		exPath+pSep+"input.list",
 		"input samples info",
 	)
+	workdir = flag.String(
+		"workdir",
+		exPath+pSep+"workdir",
+		"workdir",
+	)
 )
 
 var keyTitle = []string{
@@ -52,6 +57,69 @@ type laneInfo struct {
 type info struct {
 	SampleID string
 	LaneInfo []laneInfo
+}
+
+type pipeline struct {
+	pipelineName string
+	version      string
+	steps        []PStep
+	files        []PFile
+}
+
+type PStep struct {
+	Name          string `json:"name"`
+	first         int
+	stepFlag      int
+	computingFlag string
+	memory        int
+	threads       int
+	timeout       int
+	moduleIndex   int
+	priorStep     []string
+	nextStep      []string
+	jobSh         []PJob
+}
+
+func (a *PStep) new() {
+	a.computingFlag = "cpu"
+	a.threads = 1
+	a.timeout = 0
+}
+
+type PJob struct {
+	computingFlag string
+	mem           int
+	sh            string
+}
+
+func newPJob(mem int) (job PJob) {
+	job.computingFlag = "cpu"
+	return
+}
+
+type PFile struct {
+	fileName string
+	filePath string
+	fileType int
+	fileFlag int
+}
+
+var subDirList = []string{
+	"graph_singleBaseDepth",
+	"ExomeDepth",
+	"CNVkit",
+	"SMA",
+}
+
+var sampleDirList = []string{
+	"shell",
+	"filter1",
+	"filter2",
+	"bwa",
+	"coverage",
+	"annotation",
+	filepath.Join("gatk", "UG", "snv"),
+	filepath.Join("gatk", "HC", "short_indel"),
 }
 
 func main() {
@@ -84,6 +152,84 @@ func main() {
 		infoList[sampleID] = sampleInfo
 	}
 	log.Printf("%+v\n", infoList)
+	var samples []string
+	for k := range infoList {
+		samples = append(samples, k)
+	}
+
+	// step0 create workdir
+	err := os.MkdirAll(*workdir, 755)
+	simple_util.CheckErr(err)
+	createSubDir(*workdir, subDirList)
+	createSampleDir(*workdir, sampleDirList, samples...)
+
+	var allSteps []PStep
+	// step1 filter fq
+	var step1 = PStep{
+		Name:  "step1.filter",
+		first: 1,
+	}
+	step1.nextStep = []string{"step2.bwaMem"}
+	var step1Jobs []PJob
+	for sampleID, item := range infoList {
+		for _, lane := range item.LaneInfo {
+			var job = newPJob(10)
+			job.sh = filepath.Join(*workdir, sampleID, "shell", strings.Join([]string{"filter", lane.laneName, "sh"}, "."))
+			step1Jobs = append(step1Jobs, job)
+		}
+	}
+	step1.jobSh = step1Jobs
+	allSteps = append(allSteps, step1)
+
+	// step2 bwa mem
+	var step2 = PStep{
+		Name: "step2.bwaMem",
+	}
+	step2.priorStep = []string{"step1.filter"}
+	step2.nextStep = []string{"step3.merge"}
+	var step2Jobs []PJob
+	for sampleID, item := range infoList {
+		for _, lane := range item.LaneInfo {
+			var job = newPJob(10)
+			job.sh = filepath.Join(*workdir, sampleID, "shell", strings.Join([]string{"bwaMem", lane.laneName, "sh"}, "."))
+			step2Jobs = append(step2Jobs, job)
+		}
+	}
+	step2.jobSh = step2Jobs
+	allSteps = append(allSteps, step2)
+
+	// step3 merge bam
+	var step3 = PStep{
+		Name: "step3.merge",
+	}
+	step2.priorStep = []string{"step2.bwaMem"}
+	step2.nextStep = []string{"step3.dupMark"}
+	var step3Jobs []PJob
+	for _, sampleID := range samples {
+		var job = newPJob(10)
+		job.sh = filepath.Join(*workdir, sampleID, "shell", strings.Join([]string{"merge", "sh"}, "."))
+		step3Jobs = append(step3Jobs, job)
+	}
+	step3.jobSh = step3Jobs
+	allSteps = append(allSteps, step3)
+	log.Printf("%+v\n", allSteps)
+	simple_util.Json2File("allSteps.json", allSteps)
+}
+
+func createSubDir(workdir string, subDirList []string) {
+	for _, subdir := range subDirList {
+		err := os.MkdirAll(filepath.Join(workdir, subdir), 0755)
+		simple_util.CheckErr(err)
+	}
+}
+
+func createSampleDir(workdir string, sampleDirList []string, sampleIDs ...string) {
+	for _, sampleID := range sampleIDs {
+		for _, subdir := range sampleDirList {
+			err := os.MkdirAll(filepath.Join(workdir, sampleID, subdir), 0755)
+			simple_util.CheckErr(err)
+		}
+	}
 }
 
 func checkTitle(title []string) {
