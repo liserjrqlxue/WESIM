11e1865682644b4b7399ce267db5a535963c43e0
wangyaoshen@genomics.cn <wangyaoshen@login-25-1.yt.sz.hpc>
2019-05-16 10:55:57 +0800

    fix bwa dir

--- pipeline/script/SortSam.sh
+++ pipeline/script/SortSam.sh
@@ -4,15 +4,16 @@ pipeline=$2
 sampleID=$3
 
 Workdir=$workdir/$sampleID/bwa
+java=$pipeline/tools/java
 Picard=$pipeline/tools/picard
 samtools=$pipeline/tools/samtools
 echo Start SortSam `date`
-$pipeline/java -Djava.io.tmpdir=$workdir/javatmp \
+$java -Djava.io.tmpdir=$workdir/javatmp \
     -jar $Picard/SortSam.jar \
-    INPUT=$Workdir/bwa/$sampleID.raw.bam \
-    OUTPUT=$Workdir/bwa/$sampleID.sort.bam \
+    INPUT=$Workdir/$sampleID.raw.bam \
+    OUTPUT=$Workdir/$sampleID.sort.bam \
     SORT_ORDER=coordinate \
     VALIDATION_STRINGENCY=SILENT
 
-$samtools index $Workdir/bwa/$sampleID.sort.bam
+$samtools index $Workdir/$sampleID.sort.bam
 echo Done `date`
