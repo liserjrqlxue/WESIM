d0e7168c545d82a8b6d5776d7eb40da3935e473b
王垚燊(Yaoshen Wang) <wangyaoshen@genomics.cn>
2019-05-17 15:26:22 +0800

    change filter path

--- pipeline/script/filStat.sh
+++ pipeline/script/filStat.sh
@@ -3,13 +3,13 @@ workdir=$1
 pipeline=$2
 sampleID=$3
 laneName=$4
-Workdir=$workdir/$sampleID
+Workdir=$workdir/$sampleID/filter/$laneName
 stat=$pipeline/tools/soapnuke_stat.pl
 
 echo Start stat `date`
 perl $stat \
-    $Workdir/filter.$laneName/Basic_Statistics_of_Sequencing_Quality.txt \
-    $Workdir/filter.$laneName/Statistics_of_Filtered_Reads.txt \
-    >$Workdir/filter.$laneName/$laneName.filter.stat
+    $Workdir/Basic_Statistics_of_Sequencing_Quality.txt \
+    $Workdir/Statistics_of_Filtered_Reads.txt \
+    >$Workdir/$laneName.filter.stat
 
 echo Done `date`
--- pipeline/script/rmFQ.sh
+++ pipeline/script/rmFQ.sh
@@ -4,8 +4,8 @@ pipeline=$2
 sampleID=$3
 laneName=$4
 
-Workdir=$workdir/$sampleID
+Workdir=$workdir/$sampleID/filter/$laneName
 
 echo Start rmFQ `date`
-rm -rvf $Workdir/filter.$laneName/pe.$laneName.1_filter.fq.gz $Workdir/filter.$laneName/pe.$laneName.2_filter.fq.gz
+rm -rvf $Workdir/pe.$laneName.1_filter.fq.gz $Workdir/pe.$laneName.2_filter.fq.gz
 echo Done `date`
