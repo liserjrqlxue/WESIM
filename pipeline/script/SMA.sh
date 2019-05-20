d129937babd428cd32004459e30ef0595f973933
wangyaoshen@genomics.cn <wangyaoshen@login-25-1.yt.sz.hpc>
2019-05-20 18:02:20 +0800

    ues $PATH

--- pipeline/script/SMA.sh
+++ pipeline/script/SMA.sh
@@ -3,6 +3,7 @@ workdir=$1
 pipeline=$2
 
 Workdir=$workdir/SMA
+export PATH=$pipeline/tools:$PATH
 control=/ifs7/B2C_SGD/PROJECT/PP12_Project/ExomeDepth/workspace/SMA_WES/SMA_v2.txt.control_gene.csv
 
 time sh \
@@ -11,16 +12,16 @@ time sh \
     $pipeline/SMA_WES/PP100.gene.info.bed \
     $Workdir/coverage_v2
 
-    ls $Workdir/coverage_v2 >$Workdir/coverage.list
+ls $Workdir/coverage_v2 >$Workdir/coverage.list
 
-time $pipeline/python \
+time python \
     $pipeline/SMA_WES/SMN_copy_number_detection_v3.py \
     -b $pipeline/SMA_WES/PP100.gene.info.bed \
     -o $Workdir/SMA_v2.txt \
     -c $control \
     -l $Workdir/coverage.list
 
-time $pipeline/python \
+time python \
     $pipeline/SMA_WES/SMN_copy_number_detection_v3.py \
     -b $pipeline/SMA_WES/PP100.gene.info.bed \
     -o $Workdir/SMA_v2.noControl.txt \
