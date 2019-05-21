036c7ae9e89c3763cda851e0cdf3aa2e5cf5cab8
wangyaoshen@genomics.cn <wangyaoshen@login-25-1.yt.sz.hpc>
2019-05-21 15:21:07 +0800

    fix eDepth3

--- pipeline/script/eDepth3.sh
+++ pipeline/script/eDepth3.sh
@@ -4,9 +4,15 @@ pipeline=$2
 sampleID=$3
 gender=$4
 
+export PATH=$pipeline/tools:$PATH
 Bin=$pipeline/ExomeDepth
 outdir=$workdir/ExomeDepth
 gender=$gender
+echo `date` Start ExomeDepth3
 
-$pipeline/Rscript $Bin/run.getCNVs.R $smapleID A        $outdir
-$pipeline/Rscript $Bin/run.getCNVs.R $sampleID $gender  $outdir
+echo `date` Rscript $Bin/run.getCNVs.R $sampleID A        $outdir
+Rscript $Bin/run.getCNVs.R $sampleID A        $outdir
+echo `date` Rscript $Bin/run.getCNVs.R $sampleID $gender  $outdir
+Rscript $Bin/run.getCNVs.R $sampleID $gender  $outdir
+
+echo `date` Done
