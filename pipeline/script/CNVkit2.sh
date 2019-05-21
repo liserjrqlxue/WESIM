4053e78036235f999c1465db18668700379c3414
wangyaoshen@genomics.cn <wangyaoshen@login-25-1.yt.sz.hpc>
2019-05-21 11:14:11 +0800

    fix CNVkit2

--- pipeline/script/CNVkit2.sh
+++ pipeline/script/CNVkit2.sh
@@ -3,17 +3,17 @@ workdir=$1
 pipeline=$2
 
 Workdir=$workdir/CNVkit
-perl=$pipeline/perl
+export PATH=$pipeline/tools:$PATH
 cat $Workdir/*.gender > $Workdir/sample_gender.xls
 
-time $perl \
+time perl \
     $pipeline/CNVkit/bin/merge_result.pl \
     $Workdir/ \
     $Workdir/sample_gender.xls \
     $pipeline/CNVkit/bin/hg19_chM_male_mask.fa.fai \
     $Workdir
 
-time $perl \
+time perl \
     $pipeline/CNVkit/bin/merge_result_1K_withN.pl \
     $Workdir/CNVkit_cnv_raw.xls \
     $Workdir/sample_aneuploid.xls \
@@ -21,30 +21,30 @@ time $perl \
     $pipeline/CNVkit/bin/data/hg19.cytoBand \
     $Workdir/CNVkit_cnv.xls
 
-time $perl \
+time perl \
     $pipeline/CNV_anno/script/add_gene_OMIM.pl \
-    $workdir/CNVkit_cnv.xls \
+    $Workdir/CNVkit_cnv.xls \
     $Workdir/CNVkit_cnv_gene.xls
 
-time $perl \
+time perl \
     $pipeline/CNV_anno/script/BGI_160.pl \
     $Workdir/CNVkit_cnv_gene.xls \
     $Workdir/sample_gender.xls \
     $Workdir/CNVkit_cnv_gene_BGI160.xls
 
-time $perl \
+time perl \
     $pipeline/CNV_anno/script/CNV_anno_Clinvar_Decipher.pl \
     $Workdir/CNVkit_cnv_gene_BGI160.xls \
     $Workdir/sample_gender.xls \
     $Workdir/CNVkit_cnv_gene_BGI160_Decipher.xls
 
-time $perl \
+time perl \
     $pipeline/CNV_anno/script/add_Clinvar_DGV_BGI45W.pl \
     $Workdir/CNVkit_cnv_gene_BGI160_Decipher.xls \
     $Workdir/sample_gender.xls \
     $Workdir/CNVkit_cnv_gene_BGI160_Decipher_DGV.xls
 
-time $perl \
+time perl \
     $pipeline/CNV_anno/script/add_Pathogenicity.pl \
     $Workdir/CNVkit_cnv_gene_BGI160_Decipher_DGV.xls \
     $Workdir/CNVkit_cnv_gene_BGI160_Decipher_DGV_Pathogenicity.xls
