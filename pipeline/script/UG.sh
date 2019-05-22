90ec76281b355eaae9a2b4934ed8d6538f47368a
wangyaoshen@genomics.cn <wangyaoshen@login-25-1.yt.sz.hpc>
2019-05-22 11:37:03 +0800

    fix UG

--- pipeline/script/UG.sh
+++ pipeline/script/UG.sh
@@ -3,23 +3,24 @@ workdir=$1
 pipeline=$2
 sampleID=$3
 
+export PATH=$pipeline/tools:$PATH
 Workdir=$workdir/$sampleID
 GATK=$pipeline/tools/GenomeAnalysisTK.jar
 Bed=$pipeline/config/cns_region_hg19_bychr/for500_region.bed
-DbSNP=$pipeline/hg19/dbsnp_138.hg19.vcf
+#DbSNP=$pipeline/hg19/dbsnp_138.hg19.vcf
 hg19=$pipeline/hg19/hg19_chM_male_mask.fa
 
-$pipeline/java  -Djava.io.tmpdir=$workdir/javatmp \
+java  -Djava.io.tmpdir=$workdir/javatmp \
     -jar $GATK \
     -T UnifiedGenotyper \
     -R $hg19 \
     -L $Bed \
-    --dbsnp $DbSNP \
     --genotype_likelihoods_model BOTH \
     -stand_call_conf 30.0 \
     -stand_emit_conf 10.0 \
     -I $Workdir/bwa/$sampleID.final.bam \
     -o $Workdir/gatk/UG/$sampleID.vcf
+    #--dbsnp $DbSNP \
 
-$pipeline/bgzip -f $Workdir/gatk/UG/$sampleID.vcf
+bgzip -f $Workdir/gatk/UG/$sampleID.vcf
 
