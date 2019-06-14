0630230d834e6611e9351f7aedb0c107696c5cd2
wangyaoshen@genomics.cn <wangyaoshen@login-25-1.yt.sz.hpc>
2019-06-14 17:06:12 +0800

    fix combVcf

--- pipeline/script/combVcf.sh
+++ pipeline/script/combVcf.sh
@@ -5,13 +5,15 @@ pipeline=$2
 sampleID=$3
 
 Workdir=$workdir/$sampleID
+export PATH=$pipeline/tools:$PATH
 GATK=$pipeline/tools/GenomeAnalysisTK.jar
 IndelPhasing=$pipeline/bin/indel_phasing.pl
 hg19=$pipeline/hg19/hg19_chM_male_mask.fa
 
-$pipeline/tabix -f -p vcf $Workdir/gatk/HC/$sampleID.vcf.gz
+echo `date` Start CombineVariants
+tabix -f -p vcf $Workdir/gatk/HC/$sampleID.vcf.gz
 
-$pipeline/java  -Djava.io.tmpdir=$workdir/javatmp \
+java  -Djava.io.tmpdir=$workdir/javatmp \
     -jar $GATK \
     -T CombineVariants \
     -R $hg19 \
@@ -20,11 +22,13 @@ $pipeline/java  -Djava.io.tmpdir=$workdir/javatmp \
     --variant $Workdir/gatk/HC/short_indel/$sampleID.indel.vcf \
     -o $Workdir/gatk/$sampleID.filter.vcf
 
-$pipeline/perl $IndelPhasing \
+perl $IndelPhasing \
     -b $Workdir/bwa/$sampleID.final.bam \
     -v $Workdir/gatk/$sampleID.filter.vcf \
     -o $Workdir/gatk/$sampleID.final.vcf
 
-$pipeline/bgzip -f  $Workdir/gatk/$sampleID.final.vcf
+bgzip -f  $Workdir/gatk/$sampleID.final.vcf
 
-$pipeline/tabix -f -p vcf $Workdir/gatk/$sampleID.final.vcf.gz
+tabix -f -p vcf $Workdir/gatk/$sampleID.final.vcf.gz
+
+echo `date` Done
