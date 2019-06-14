39bb42604f556963fdae3cd6af8737ecbeac9c44
wangyaoshen@genomics.cn <wangyaoshen@login-25-1.yt.sz.hpc>
2019-06-14 15:41:59 +0800

    fix

--- pipeline/script/dSurvey.sh
+++ pipeline/script/dSurvey.sh
@@ -9,10 +9,11 @@ export PATH=$pipeline/tools:$PATH
 #Bin=$pipeline/bin
 #UncoverdRegionSjt=$Bin/uncovered_region_sjt.pl
 #Bam2depths=$pipeline/bam2depths_shiquan_mgiseq2000.pl
-#GenderCorrect=$pipeline/XY_gender_correct.pl
 
+GenderCorrect=$pipeline/tools/XY_gender_correct.pl
 RegionDir=$pipeline/config/coverage_region_hg19_bychr/
 Region=$RegionDir/for500_all_region
+tag=BGI59M
 
 
 echo `date` Start depthSurvey
@@ -20,6 +21,7 @@ echo `date` Start depthSurvey
 #samtools view -u $Workdir/bwa/$sampleID.final.bam chrX >$Workdir/bwa/chrX.sort.bam
 #samtools view -u $Workdir/bwa/$sampleID.final.bam chrY >$Workdir/bwa/chrY.sort.bam
 
+echo `date` bamdst -p $Region --uncover 5 -o $Workdir/coverage $Workdir/bwa/$sampleID.final.bam --cutoffdepth 20
 bamdst -p $Region --uncover 5 -o $Workdir/coverage $Workdir/bwa/$sampleID.final.bam --cutoffdepth 20
 
 #$Bam2depths --bamdir=$Workdir/bwa --region=$RegionDir --out=$Workdir/coverage --flank=100
@@ -27,6 +29,7 @@ bamdst -p $Region --uncover 5 -o $Workdir/coverage $Workdir/bwa/$sampleID.final.
 #$pipeline/Rscript $Bin/cumn.R $Workdir/coverage/cumu_target.plot $sampleID $workdir/graph_sigleBaseDepth/$sampleID_Cumu.png
 #$pipeline/perl $UncoverdRegionSjt $RegionDir/all_region  $Workdir/coverage/target.detail $Workdir/coverage
 #$pipeline/gzip -f $Workdir/coverage/target.detail
-#$pipeline/perl $GenderCorrect $Workdir/coverage/chromosomes.report $Region
+echo `date` perl $GenderCorrect $Workdir/coverage/chromosomes.report $tag
+perl $GenderCorrect $Workdir/coverage/chromosomes.report $tag
 
 echo `date` Done
