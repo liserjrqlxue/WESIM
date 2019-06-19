20361fa29fa39d1a746cd2fa1440d9bd85bceec8
wangyaoshen@genomics.cn <wangyaoshen@login-25-1.yt.sz.hpc>
2019-06-19 23:25:29 +0800

    index before bqsr

---
+++ pipeline/script/indxDup.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+workdir=$1
+pipeline=$2
+sampleID=$3
+
+Workdir=$workdir/$sampleID/bwa
+export PATH=$pipeline/tools:$PATH
+
+echo `date` Start IndexDupBam
+samtools index $Workdir/$sampleID.sort.dup.bam
+
+echo `date` Done
