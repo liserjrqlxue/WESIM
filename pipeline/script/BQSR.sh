97bce06c776849e2052d50c952cd8a6313a898d9
wangyaoshen@genomics.cn <wangyaoshen@login-25-1.yt.sz.hpc>
2019-06-19 23:17:04 +0800

    use gatk4 BaseRecalibrator

--- pipeline/script/BQSR.sh
+++ pipeline/script/BQSR.sh
@@ -5,19 +5,22 @@ sampleID=$3
 
 Workdir=$workdir/$sampleID/bwa
 export PATH=$pipeline/tools:$PATH
-GATK=$pipeline/tools/GenomeAnalysisTK.jar
 Bed=$pipeline/config/cns_region_hg19_bychr/for500_region.bed
 DbSNP=$pipeline/hg19/dbsnp_138.hg19.vcf
+GoldIndels=$pipeline/hg19/Mills_and_1000G_gold_standard.indels.hg19.sites.vcf
 hg19=$pipeline/hg19/hg19_chM_male_mask.fa
 
-echo Start BQSR `date`
-java  -Djava.io.tmpdir=$workdir/javatmp \
-    -jar $GATK \
-    -T BaseRecalibrator \
-    -R $hg19 \
-    -L $Bed \
-    -knownSites $DbSNP \
-    -I $Workdir/$sampleID.sort.realn.bam \
-    -o $Workdir/$sampleID.recal_data.grp
+echo `date` Start BQSR
+mkdir -p $workdir/javatmp
+gatk \
+  BaseRecalibrator \
+  --tmp-dir=$workdir/javatmp \
+  -I $Workdir/$sampleID.sort.dup.bam \
+  -O $Workdir/$sampleID.recal_data.grp \
+  --known-sites $DbSNP \
+  --known-sites $GoldIndels \
+  -R $hg19 \
+  -L $Bed \
+  --showHidden
 
-echo Done `date`
+echo `date` Done
