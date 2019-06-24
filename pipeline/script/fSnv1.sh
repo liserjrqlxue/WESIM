65e7b58ebb04e1905e3e703dfb0619620fa9a3d3
wangyaoshen@genomics.cn <wangyaoshen@login-25-1.yt.sz.hpc>
2019-06-24 15:37:12 +0800

    use GATK4 for SNV filter

--- pipeline/script/fSnv1.sh
+++ pipeline/script/fSnv1.sh
@@ -5,18 +5,18 @@ sampleID=$3
 
 Workdir=$workdir/$sampleID
 export PATH=$pipeline/tools:$PATH
-GATK=$pipeline/tools/GenomeAnalysisTK.jar
 hg19=$pipeline/hg19/hg19_chM_male_mask.fa
 
 echo `date` Start SelectVariantsSNP
-tabix -f -p vcf $Workdir/gatk/UG/$sampleID.vcf.gz
 
-java  -Djava.io.tmpdir=$workdir/javatmp \
-    -jar $GATK \
-    -T SelectVariants \
-    -R $hg19 \
-    -selectType SNP \
-    --variant $Workdir/gatk/UG/$sampleID.vcf.gz \
-    -o        $Workdir/gatk/UG/snv/$sampleID.snv.raw.vcf
+gatk \
+  SelectVariants \
+  --tmp-dir=$workdir/javatmp \
+  -O $Workdir/gatk/$sampleID.snp.raw.vcf \
+  -V $Workdir/gatk/$sampleID.vcf.vcf.gz \
+  -R $hg19 \
+  -select-type SNP \
+  --showHidden
+
 
 echo `date` Done
