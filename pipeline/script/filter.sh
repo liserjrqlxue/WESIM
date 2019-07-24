daa77fea0f930e77144b7f8e3a68f3581e0df32f
Wang Yaoshen <wangyaoshen@bgi.com>
2019-07-24 11:32:49 +0800

    echo succes or err

--- pipeline/script/filter.sh
+++ pipeline/script/filter.sh
@@ -5,17 +5,20 @@ sampleID=$3
 laneName=$4
 fq1=$5
 fq2=$6
+
 Workdir=$workdir/$sampleID
-SOAPnuke=$pipeline/tools/SOAPnuke
+export PATH=$pipeline/tools:$PATH
 
 echo Start SOAPnuke `date`
-$SOAPnuke filter -o $Workdir/filter/$laneName \
+SOAPnuke filter -o $Workdir/filter/$laneName \
     --fq1 $fq1 \
     --fq2 $fq2 \
     --adapter1 AAGTCGGAGGCCAAGCGGTCTTAGGAAGACAA \
     --adapter2 AAGTCGGATCGTAGCCATGTCGTTCTGTGAGCCAAGGAGTTG \
-    --cleanFq1 pe.$laneName.1_filter.fq.gz \
-    --cleanFq2 pe.$laneName.2_filter.fq.gz \
-    --nRate 0.05 --lowQual 10  --seqType 0 --qualRate 0.5 -Q 2 -G 2
+    --cleanFq1 $sampleID.$laneName.filter_1.fq.gz \
+    --cleanFq2 $sampleID.$laneName.filter_2.fq.gz \
+    --nRate 0.05 --lowQual 10  --seqType 0 --qualRate 0.5 -Q 2 -G 2 \
+&& echo succes || echo error
+
 
 echo Done `date`
