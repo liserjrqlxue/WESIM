204dc36b769f48871917fdf6a59d4f708213ac8c
Wang Yaoshen <wangyaoshen@bgi.com>
2019-07-24 17:27:30 +0800

    add MergeSamFiles.sh

---
+++ pipeline/script/MergeSamFiles.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+workdir=$1
+pipeline=$2
+sampleID=$3
+
+Workdir=$workdir/$sampleID/bwa
+export PATH=$pipeline/tools:$PATH
+
+echo `date` Start MergeSamFiles $sampleID
+shift 3
+inputBams=""
+INPUT=""
+for i in $@;do
+    inputBams="$inputBams $Workdir/$sampleID.raw.$i.bam"
+    INPUT="$INPUT -I $Workdir/$sampleID.raw.$i.bam"
+done
+
+time gatk \
+  MergeSamFiles \
+  --TMP_DIR $Workdir/javatmp \
+  $INPUT \
+  -O $Workdir/$sampleID.merge.bam \
+  --USE_THREADING \
+  -SO coordinate \
+  --showHidden=true \
+&& echo rm -rvf $inputBams || echo error
+
+echo `date` Done
