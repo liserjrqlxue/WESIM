1a4a69dbe79df7103f514ec0e6cca639d6dbc9b6
Wang Yaoshen <wangyaoshen@bgi.com>
2019-07-24 11:46:24 +0800

    index dup.bam instead of sort.dup.bam

--- pipeline/script/indxDup.sh
+++ pipeline/script/indxDup.sh
@@ -7,6 +7,6 @@ Workdir=$workdir/$sampleID/bwa
 export PATH=$pipeline/tools:$PATH
 
 echo `date` Start IndexDupBam
-samtools index $Workdir/$sampleID.sort.dup.bam
+samtools index $Workdir/$sampleID.dup.bam
 
 echo `date` Done
