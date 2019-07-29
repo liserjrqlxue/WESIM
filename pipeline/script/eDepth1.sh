93c996a4bd50963f3fbb51a15788ed4616e7797e
Wang Yaoshen <wangyaoshen@bgi.com>
2019-07-29 17:53:22 +0800

    use gender from coverage/gender.txt for exomeDepth

--- pipeline/script/eDepth1.sh
+++ pipeline/script/eDepth1.sh
@@ -2,14 +2,17 @@
 workdir=$1
 pipeline=$2
 sampleID=$3
-gender=$4
 
+Workdir=$workdir/$sampleID
 export PATH=$pipeline/tools:$PATH
 Bin=$pipeline/ExomeDepth
-Bam=$workdir/$sampleID/bwa/$sampleID.bqsr.bam
-outdir=$workdir/ExomeDepth
+Bam=$Workdir/bwa/$sampleID.bqsr.bam
+outdir=$Workdir/bwa
+
+grep "gender Male" $Workdir/coverage/gender.txt && gender=M||gender=F
 
 echo `date` Start getBamCount
-Rscript $Bin/run.getBamCount.R $sampleID $Bam A       $outdir $Bin
-Rscript $Bin/run.getBamCount.R $sampleID $Bam $gender $outdir $Bin
+echo $sampleID gender:$gender
+time Rscript $Bin/run.getBamCount.R $sampleID $Bam A       $outdir $Bin
+time Rscript $Bin/run.getBamCount.R $sampleID $Bam $gender $outdir $Bin
 echo `date` Done
