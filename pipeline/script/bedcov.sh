f2a498a67d3f11976a75cbee405f2b4b54692129
Wang Yaoshen <wangyaoshen@bgi.com>
2019-07-29 17:52:26 +0800

    output depth and bedcov to cnv/

--- pipeline/script/bedcov.sh
+++ pipeline/script/bedcov.sh
@@ -3,16 +3,18 @@ workdir=$1
 pipeline=$2
 sampleID=$3
 
-Workdir=$workdir/$sampleID/bwa
+Workdir=$workdir/$sampleID
 export PATH=$pipeline/tools:$PATH
 
 hg19=$pipeline/hg19/hg19_chM_male_mask.fa
 bed=$pipeline/SMA_WES/PP100.gene.info.bed
 
 echo `date` Start samtoolsBedcov
-samtools \
+mkdir -p $Workdir/cnv
+echo -e "Chr\tStart\tEnd\tStrand\tGene\tExon\tTrans\tPrimarytag\tGenelength\t${sampleID}_total_cvg" > $Workdir/cnv/$sampleID.bqsr.bam.bedcov
+time samtools \
   bedcov \
   $bed \
-  $Workdir/$sampleID.bqsr.bam \
-  > $Workdir/$sampleID.bqsr.bam.bedcov
+  $Workdir/bwa/$sampleID.bqsr.bam \
+  >> $Workdir/cnv/$sampleID.bqsr.bam.bedcov
 echo `date` Done
--- pipeline/script/depth.sh
+++ pipeline/script/depth.sh
@@ -3,16 +3,18 @@ workdir=$1
 pipeline=$2
 sampleID=$3
 
-Workdir=$workdir/$sampleID/bwa
+Workdir=$workdir/$sampleID
 export PATH=$pipeline/tools:$PATH
 
 hg19=$pipeline/hg19/hg19_chM_male_mask.fa
 bed=$pipeline/SMA_WES/PP100.gene.info.bed
 
 echo `date` Start samtoolsDepth
-samtools \
-  depth -a \
+mkdir -p $Workdir/cnv
+echo -e "Chr\tPos\tDepth_for_${sampleID}" >$Workdir/cnv/$sampleID.bqsr.bam.depth
+time samtools \
+  depth -aa \
   -b $bed \
-  $Workdir/$sampleID.bqsr.bam \
-  > $Workdir/$sampleID.bqsr.bam.depth
+  $Workdir/bwa/$sampleID.bqsr.bam \
+  >> $Workdir/cnv/$sampleID.bqsr.bam.depth
 echo `date` Done
