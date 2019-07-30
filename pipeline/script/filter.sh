4c0c7b5ef57f79c2bbb18564cdb1bd10bf6c56e5
Wang Yaoshen <wangyaoshen@bgi.com>
2019-07-30 17:17:03 +0800

    merge filStat to filter

--- pipeline/script/filter.sh
+++ pipeline/script/filter.sh
@@ -6,11 +6,12 @@ laneName=$4
 fq1=$5
 fq2=$6
 
-Workdir=$workdir/$sampleID
+Workdir=$workdir/$sampleID/filter/$laneName
 export PATH=$pipeline/tools:$PATH
+stat=$pipeline/tools/soapnuke_stat.pl
 
-echo Start SOAPnuke `date`
-time SOAPnuke filter -o $Workdir/filter/$laneName \
+echo `date` Start SOAPnuke
+time SOAPnuke filter -o $Workdir \
     --fq1 $fq1 \
     --fq2 $fq2 \
     --adapter1 AAGTCGGAGGCCAAGCGGTCTTAGGAAGACAA \
@@ -20,5 +21,11 @@ time SOAPnuke filter -o $Workdir/filter/$laneName \
     --nRate 0.05 --lowQual 10  --seqType 0 --qualRate 0.5 -Q 2 -G 2 \
 && echo succes || echo error
 
+echo `date` Start FQstat
+time perl $stat \
+    $Workdir/Basic_Statistics_of_Sequencing_Quality.txt \
+    $Workdir/Statistics_of_Filtered_Reads.txt \
+    >$Workdir/$laneName.filter.stat \
+&& echo succes || echo error
 
-echo Done `date`
+echo `date` Done
