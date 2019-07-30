adc6ec0db602fb3799da6c9a823840b39fd9494a
Wang Yaoshen <wangyaoshen@bgi.com>
2019-07-30 16:50:16 +0800

    fix error of eReport

--- pipeline/script/eReport.sh
+++ pipeline/script/eReport.sh
@@ -9,19 +9,21 @@ export PATH=$pipeline/tools:$PATH
 prefix=$Workdir/annotation/$sampleID
 
 echo `date` Start anno2xlsx
-anno2xlsx \
+time anno2xlsx \
   -prefix $workdir/result/$sampleID \
   -snv $prefix.out.ACMG.updateFunc \
   -qc  $Workdir/coverage/coverage.report \
   -large $Workdir/cnv/CNVkit_cnv_gene_BGI160_Decipher_DGV_Pathogenicity.xls \
   -exon $Workdir/cnv/$sampleID.CNV.calls.anno \
-  -smn $Workdir/cnv/$sampleID.SMA_v2.txt 
+  -smn $Workdir/cnv/$sampleID.SMA_v2.txt \
   -wesim \
   -list $sampleID \
+  && echo success || echo error
 
-for i in $workdir/result/$preoband.*Tier1*xlsx $workdir/result/$preoband.*Tier2*xlsx;do
-  echo xlsx2txt -xlsx $i
-  xlsx2txt -xlsx $i
+for i in $workdir/result/$sampleID.*Tier1*xlsx $workdir/result/$sampleID.*Tier2*xlsx;do
+  echo `date` xlsx2txt -xlsx $i
+  time xlsx2txt -xlsx $i \
+    && echo success || echo error
 done
 
 echo `date` Done
