4e5f4a0297cd7142d0ef189b6f17fb870360e234
Wang Yaoshen <wangyaoshen@bgi.com>
2019-08-29 16:44:07 +0800

    add productCode to anno2xlsx

--- pipeline/script/eReport.sh
+++ pipeline/script/eReport.sh
@@ -2,18 +2,18 @@
 workdir=$1
 pipeline=$2
 sampleID=$3
+productCode=$4
 
 grep -P "$sampleID\tpass" $workdir/result/$sampleID/$sampleID.QC.txt \
 || { echo `date` sample QC not pass, skip $0;exit 0; }
 
 Workdir=$workdir/$sampleID
 export PATH=$pipeline/tools:$PATH
-prefix=$Workdir/annotation/$sampleID
 
 echo `date` Start anno2xlsx
 time anno2xlsx \
-  -prefix $workdir/result/$sampleID \
-  -snv $prefix.out.updateFunc \
+  -prefix $Workdir/$sampleID \
+  -snv $Workdir/annotation/$sampleID.out.updateFunc \
   -qc  $Workdir/coverage/coverage.report \
   -large $Workdir/cnv/CNVkit_cnv_gene_BGI160_Decipher_DGV_Pathogenicity.xls \
   -exon $Workdir/cnv/$sampleID.CNV.calls.anno \
@@ -22,14 +22,12 @@ time anno2xlsx \
   -acmg \
   -redis -redisAddr 127.0.0.1:6380 \
   -list $sampleID \
+  -product $productCode \
 && echo success \
 || { echo error;exit 1; }
 
-for i in $workdir/result/$sampleID.*Tier1*xlsx $workdir/result/$sampleID.*Tier2*xlsx;do
-  echo `date` xlsx2txt -xlsx $i
-  time xlsx2txt -xlsx $i \
-  && echo success \
-  || { echo error;exit 1; }
-done
+echo cp -v $Workdir/$sampleID.qc.tsv $workdir/result/$sampleID/
+cp -v $Workdir/$sampleID.qc.tsv $workdir/result/$sampleID/ \
+|| { echo error;exit 1; }
 
 echo `date` Done
