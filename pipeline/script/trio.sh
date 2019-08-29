c17d261ef5e5d06ca76bcdbb8907e54263c7f40a
Wang Yaoshen <wangyaoshen@bgi.com>
2019-08-29 16:47:31 +0800

    add appendix excel

--- pipeline/script/trio.sh
+++ pipeline/script/trio.sh
@@ -46,43 +46,73 @@ time \
   $workdir/$proband/$subdir/$proband.$suffix \
   $workdir/$father/$subdir/$father.$suffix \
   $workdir/$mother/$subdir/$mother.$suffix \
-&&echo `date` family merge done
+&& echo success \
+|| { echo error;exit 1; }
 
 echo `date` TrioAnno2Xlsx
 
 anno2xlsx \
-  -prefix $workdir/result/$proband \
+  -prefix $Workdir/$proband \
   -wesim \
   -trio \
   -list $proband,$father,$mother \
-  -snv $workdir/$proband.family.$suffix \
+  -snv $workdir/$proband/$proband.family.$suffix \
   -qc  $workdir/$proband/$qc,$workdir/$father/$qc,$workdir/$mother/$qc \
   -large $workdir/$proband/$CNVkit,$workdir/$father/$CNVkit,$workdir/$mother/$CNVkit \
   -exon $workdir/$proband/cnv/$proband.$exon,$workdir/$father/cnv/$father.$exon,$workdir/$mother/cnv/$mother.$exon \
   -smn $workdir/$proband/cnv/$proband.$SMA,$workdir/$father/cnv/$father.$SMA,$workdir/$mother/cnv/$mother.$SMA \
-&& echo success || echo error
+&& echo success \
+|| { echo error;exit 1; }
 
-for i in $workdir/result/$preoband.*Tier1*xlsx $workdir/result/$preoband.*Tier2*xlsx;do
-  echo xlsx2txt -xlsx $i
-  xlsx2txt -xlsx $i
-done
-
-
-echo `date` score for family
+echo `date` score for family $proband
 sampleID=$proband
 export PATH=/home/uploader/anaconda3/bin:$PATH
 source /home/uploader/anaconda3/etc/profile.d/conda.sh
 conda activate base
 
 mkdir -p $Workdir/score/inputData/file
-gzip -dc $workdir/$proband/gatk/$sampleID.filter.vcf.gz > $Workdir/score/inputData/file/$sampleID.filter.vcf
-for i in $workdir/result/$sampleID.*Tier1*.xlsx;do
-  echo ln -sf $i $workdir/score/inputData/file/$sampleID.Tier1.xlsx
-  ln -sf $i $workdir/score/inputData/file/$sampleID.Tier1.xlsx
+gzip -dc $Workdir/gatk/$sampleID.filter.vcf.gz > $Workdir/score/inputData/file/$sampleID.filter.vcf \
+  && echo success \
+  || { echo error;exit 1; }
+for i in $Workdir/$sampleID.Tier1*.xlsx;do
+  echo cp $i $Workdir/score/inputData/file/$sampleID.Tier1.xlsx
+  rm -rvf $Workdir/score/inputData/file/$sampleID.Tier1.xlsx && cp -v $i $Workdir/score/inputData/file/$sampleID.Tier1.xlsx \
+  && echo success \
+  || { echo error;exit 1; }
 done
-echo $HPO > $workdir/score/inputData/file/hpo.txt
-cat <<< "{\"input_files\":[\"$sampleID.Tier1.xlsx\",\"$sampleID.filter.vcf\"],\"action_type\":4,\"project_name\":\"test\",\"sample_name\":\"$sampleID\"}" >$workdir/score/input.json
+echo $HPO > $Workdir/score/inputData/file/hpo.txt
+cat <<< "{\"input_files\":[\"$sampleID.Tier1.xlsx\",\"$sampleID.filter.vcf\"],\"action_type\":4,\"project_name\":\"test\",\"sample_name\":\"$sampleID\"}" >$Workdir/score/input.json
 
-time python /home/uploader/uploader-WES/score/sample_score/run_three_uploader.py -i $workdir/score/input.json
-xlsx2txt -xlsx $workdir/score/outputData/file/Result_new_$sampleID.Tier1.xlsx -prefix result/$sampleID.score.Tier1
+echo `date` start score
+time python /home/uploader/uploader-WES/score/sample_score/run_three_uploader.py -i $Workdir/score/input.json \
+&& echo success \
+|| { echo error;exit 1; }
+
+echo `date` create appendix
+python3 $pipeline/wes-auto-report/generate-report.py $sampleID $workdir/sample.info $Workdir/score/outputData/file $workdir/result/$sampleID \
+&& echo success \
+|| { echo error;exit 1; }
+
+echo `date` create $sampleID.reult.tsv
+Tier1toResult -xlsx $Workdir/score/outputData/file/Result_new_$sampleID.Tier1.xlsx -trio -prefix $workdir/result/$sampleID/$sampleID \
+&& echo success \
+|| { echo error;exit 1; }
+
+
+echo `date` copy Tier1.xlsx and qc.tsv to result
+
+echo cp -v $Workdir/$sampleID.qc.tsv $workdir/result/$sampleID/
+cp -v $Workdir/$sampleID.qc.tsv $workdir/result/$sampleID/ \
+|| { echo error;exit 1; }
+
+echo cp -v $Workdir/score/outputData/file/Result_new_$sampleID.Tier1.xlsx $workdir/result/$sampleID/$sampleID.score.Tier1.xlsx
+cp -v $Workdir/score/outputData/file/Result_new_$sampleID.Tier1.xlsx $workdir/result/$sampleID/$sampleID.score.Tier1.xlsx \
+|| { echo error;exit 1; }
+
+echo xlsx2txt -xlsx $workdir/result/$sampleID/$sampleID.score.Tier1.xlsx
+xlsx2txt -xlsx $workdir/result/$sampleID/$sampleID.score.Tier1.xlsx \
+|| { echo error;exit 1; }
+#done
+
+
 echo `date` Done
