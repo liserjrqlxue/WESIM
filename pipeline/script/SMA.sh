d52adadfcf58a486a4279bffca8330fe0b4d15d3
Wang Yaoshen <wangyaoshen@bgi.com>
2019-08-29 16:40:27 +0800

    check and skip bedcov and depth

--- pipeline/script/SMA.sh
+++ pipeline/script/SMA.sh
@@ -14,34 +14,38 @@ bed=$pipeline/SMA_WES/PP100.gene.info.bed
 control=$pipeline/SMA_WES/SMA_v2.txt.control_gene.csv
 
 mkdir -p $Workdir/cnv
-echo `date` Start samtoolsBedcov
-echo -e "Chr\tStart\tEnd\tStrand\tGene\tExon\tTrans\tPrimarytag\tGenelength\t${sampleID}_total_cvg" > $Workdir/cnv/$sampleID.bqsr.bam.bedcov \
-&& echo success \
-|| { echo error;exit 1; }
 
-time samtools \
-  bedcov \
-  $bed \
-  $Workdir/bwa/$sampleID.bqsr.bam \
-  >> $Workdir/cnv/$sampleID.bqsr.bam.bedcov \
-&& echo success \
-|| { echo error;exit 1; }
+echo `date` Start samtoolsBedcov
+if [ ! -s "$Workdir/cnv/$sampleID.bqsr.bam.bedcov" ];then
+	echo -e "Chr\tStart\tEnd\tStrand\tGene\tExon\tTrans\tPrimarytag\tGenelength\t${sampleID}_total_cvg" > $Workdir/cnv/$sampleID.bqsr.bam.bedcov \
+	|| { echo error;exit 1; }
+	time samtools \
+	  bedcov \
+	  $bed \
+	  $Workdir/bwa/$sampleID.bqsr.bam \
+	  >> $Workdir/cnv/$sampleID.bqsr.bam.bedcov \
+	&& echo success \
+	|| { echo error;exit 1; }
+fi
 
 echo `date` Start samtoolsDepth
-echo -e "Chr\tPos\tDepth_for_${sampleID}" >$Workdir/cnv/$sampleID.bqsr.bam.depth
-time samtools \
-  depth -aa \
-  -b $bed \
-  $Workdir/bwa/$sampleID.bqsr.bam \
-  >> $Workdir/cnv/$sampleID.bqsr.bam.depth \
-&& echo success \
-|| { echo error;exit 1; }
+if [ ! -s "$Workdir/cnv/$sampleID.bqsr.bam.depth" ];then
+	echo -e "Chr\tPos\tDepth_for_${sampleID}" >$Workdir/cnv/$sampleID.bqsr.bam.depth \
+	|| { echo error;exit 1; }
+	time samtools \
+	  depth -aa \
+	  -b $bed \
+	  $Workdir/bwa/$sampleID.bqsr.bam \
+	  >> $Workdir/cnv/$sampleID.bqsr.bam.depth \
+	&& echo success \
+	|| { echo error;exit 1; }
+fi
 
 echo `date` Start SMA
 time python2 $pipeline/SMA_WES/v1/SMN_copy_number_detection_v3.single.py \
   -b $pipeline/SMA_WES/PP100.gene.info.bed \
   -o $Workdir/cnv/$sampleID.SMA_v2.txt \
-  -l $Workdir/cnv/$sampleID.bqsr.bam.depth \
+  -l $Workdir/cnv/$sampleID.bqsr.bam \
   -c $control \
 && echo success \
 || { echo error;exit 1; }
