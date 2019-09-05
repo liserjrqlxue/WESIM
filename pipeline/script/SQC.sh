d6cabb89d20b5edf43d5a75c098b3400f227b348
Wang Yaoshen <wangyaoshen@bgi.com>
2019-09-05 09:35:45 +0800

    add chipCode to standard.QC.txt

--- pipeline/script/SQC.sh
+++ pipeline/script/SQC.sh
@@ -2,13 +2,14 @@
 workdir=$1
 pipeline=$2
 sampleID=$3
-StandardTag=${4:-N}
+chipCode=$4
+StandardTag=${5:-N}
 
 Workdir=$workdir/result
 export PATH=$pipeline/tools:$PATH
 
 if [ $StandardTag == "Y" ];then
-	echo -e "$sampleID\tPASS" >> $Workdir/standard.QC.txt \
+	echo -e "$sampleID\t$chipCode\tPASS" >> $Workdir/standard.QC.txt \
 	&& echo success \
 	|| { echo error;exit 1; }
 fi
