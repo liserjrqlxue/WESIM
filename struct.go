f1fb600fd2c66d3d98064865fcc6aaf262dba900
王垚燊(Yaoshen Wang) <wangyaoshen@genomics.cn>
2020-06-18 09:18:53 +0800

    use local WrapSubmit as modify and test other package is not vary easy

--- struct.go
+++ struct.go
@@ -3,10 +3,12 @@ package main
 import (
 	"log"
 	"math/rand"
+	"os/exec"
+	"path"
+	"regexp"
 	"strings"
 	"time"
 
-	"github.com/liserjrqlxue/goUtil/sge"
 	"github.com/liserjrqlxue/libIM"
 	//"github.com/liserjrqlxue/goUtil/sge"
 )
@@ -33,11 +35,37 @@ func submitJob(job *libIM.Job, throttle chan bool) {
 	log.Printf("start\t[%s]:[%s]:[%s]", job.Step.Name, job.Id, job.Sh)
 	var jid = job.Id
 	if *submit != "" {
-		jid = sge.WrapSubmit(*submit, job.Sh, strings.Join(hjid, ","), job.SubmitArgs)
+		jid = WrapSubmit(*submit, job.Sh, strings.Join(hjid, ","), job.SubmitArgs)
 	} else {
 		time.Sleep(time.Duration(rand.Int63n(10)) * time.Second)
 	}
 	job.Done(jid)
 	log.Printf("finish\t[%s]:[%s]:[%s]", job.Step.Name, job.Id, jid)
 	<-throttle
+}
+
+var sgeJobId = regexp.MustCompile(`^Your job (\d+) \("\S+"\) has been submitted\n$`)
+
+func WrapSubmit(submit, script, hjid string, submitArgs []string) (jid string) {
+	if hjid != "" {
+		submitArgs = append(submitArgs, "-hold_jid", hjid)
+	}
+	var outDir = path.Dir(script)
+	submitArgs = append(submitArgs, "-e", outDir, "-o", outDir)
+	var cmds = append(submitArgs, script)
+	var c = exec.Command(submit, cmds...)
+	log.Printf("%s [%s]", submit, strings.Join(cmds, "]["))
+	var submitLogBytes, err = c.CombinedOutput()
+	if err != nil {
+		log.Fatalf("Error: %v:[%v]", err, submitLogBytes)
+	}
+	// Your job (\d+) \("script"\) has been submitted
+	log.Print(submitLogBytes)
+	var submitLogs = sgeJobId.FindStringSubmatch(string(submitLogBytes))
+	if len(submitLogs) == 2 {
+		jid = submitLogs[1]
+	} else {
+		log.Fatalf("Error: jid parse error:%v->%v", submitLogBytes, submitLogs)
+	}
+	return
 }
