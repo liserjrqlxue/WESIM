2f2313a32f93732c031c49d40d7bc6530291d6fb
Wang Yaoshen <wangyaoshen@bgi.com>
2021-01-27 17:29:01 +0800

    set default value of QChistory and add complete

--- pipeline/script/dSurvey.sh
+++ pipeline/script/dSurvey.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 workdir=$1
 pipeline=$2
 sampleID=$3
-QChistory=$4
+QChistory=${4:-""}
 
 complete=$workdir/$sampleID/shell/dSurvey.sh.complete
 if [ -e "$complete" ];then
@@ -24,15 +24,20 @@ tag=BGI59M
 
 echo `date` Start depthSurvey
 
-echo `date` bamdst -p $Region --uncover 5 -o $Workdir/coverage $Workdir/bwa/$sampleID.bqsr.bam --cutoffdepth 20
-\time -v bamdst -p $Region --uncover 5 -o $Workdir/coverage $Workdir/bwa/$sampleID.bqsr.bam --cutoffdepth 20 
+bamdstComplete=$workdir/$sampleID/coverage/bamdst.complete
+if [ -e "$bamdstComplete" ];then
+	echo "$bamdstComplete and skip"
+else
+	echo `date` bamdst -p $Region --uncover 5 -o $Workdir/coverage $Workdir/bwa/$sampleID.bqsr.bam --cutoffdepth 20
+	\time -v bamdst -p $Region --uncover 5 -o $Workdir/coverage $Workdir/bwa/$sampleID.bqsr.bam --cutoffdepth 20 
+	touch $bamdstComplete
+fi
 
 echo `date` perl $GenderCorrect $Workdir/coverage/chromosomes.report $tag
-\time -v perl $GenderCorrect $Workdir/coverage/chromosomes.report $tag
+perl $GenderCorrect $Workdir/coverage/chromosomes.report $tag
 
 echo `date` perl $GetQC $sampleID $Workdir $Workdir $QChistory
-
-\time -v perl $GetQC $sampleID $Workdir $Workdir $QChistory 
+perl $GetQC $sampleID $Workdir $Workdir $QChistory 
 
 echo `date` Done
 
