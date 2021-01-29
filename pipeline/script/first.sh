90c64f566ab0242880a009883ce4a4ee34e09f60
Wang Yaoshen <wangyaoshen@bgi.com>
2021-01-29 17:12:13 +0800

    touch complete for first.sh

--- pipeline/script/first.sh
+++ pipeline/script/first.sh
@@ -5,6 +5,12 @@ workdir=$1
 pipeline=$2
 laneInput=${3:-null}
 
+complete=$workdir/shell/first.sh.complete
+if [ -e "$complete" ];then
+	echo "$complete and skip"
+	exit 0
+fi
+
 Workdir=$workdir/result
 export PATH=$pipeline/tools:$PATH
 GetLaneQC=$pipeline/tools/getQC/get.lane.QC.batch.pl
@@ -13,6 +19,7 @@ echo -e "sampleID\tQC\tchip_code" > $Workdir/standard.QC.txt
 
 if [ $laneInput == "null" ];then
 	echo no laneInput
+	touch $complete
 	exit 0
 fi
 
@@ -20,5 +27,5 @@ echo `date` Start GetLaneQC
 
 \time -v perl $GetLaneQC $laneInput $Workdir
 
-touch $workdir/shell/first.sh.complete
 echo `date` Done
+touch $complete
