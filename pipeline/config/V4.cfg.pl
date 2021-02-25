7bfb256049fd50756e38023242d97499646896f8
Wang Yaoshen <wangyaoshen@bgi.com>
2021-02-25 10:39:12 +0800

    add V4 cfg

---
+++ pipeline/config/V4.cfg.pl
@@ -0,0 +1,8 @@
+%config = (
+	run_type    => "single",
+	TGP_POP     => "EAS",
+	FLANK_LEN   => 5,
+	trans       => $Bin . '/../etc/BGI59M_CG.3585.NM.gene.HGMD.LRG.NM',
+	Headers     => $Bin . '/../etc/header.list',
+	HomoRef_Var => $Bin . '/../etc/PP_homoRef_Var.list',
+);
